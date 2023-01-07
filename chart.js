12f3a3be38bdfff4b29dd688cb7f5d901ef29a7e
LinJing-Liu <linjingl93@gmail.com>
2023-01-07 13:49:17 -0500

    update chart styling

--- chart.js
+++ chart.js
@@ -1,34 +1,42 @@
 anychart.onDocumentReady(function() {
 
-// set the data
-chrome.storage.local.get("prodTime").then((result) => {
-    let prodTime = result.prodTime;
-    if (prodTime == null) {
-        prodTime = 0;
-    }
-
-    chrome.storage.local.get("unprodTime").then((result) => {
-        let unprodTime = result.unprodTime;
-        if (unprodTime == null) {
-            unprodTime = 0;
+    // set the data
+    chrome.storage.local.get("prodTime").then((result) => {
+        let prodTime = result.prodTime;
+        if (prodTime == null) {
+            prodTime = 0;
         }
-        var data = [
-            {x: "productive", value: prodTime/(prodTime + unprodTime)}, 
-            {x: "unproductive", value: unprodTime/(prodTime + unprodTime)},
-        ];
 
-        // create the chart
-        var chart = anychart.pie();
+        chrome.storage.local.get("unprodTime").then((result) => {
+            let unprodTime = result.unprodTime;
+            if (unprodTime == null) {
+                unprodTime = 0;
+            }
+            var data = [
+                {
+                    x: "productive", 
+                    value: prodTime/(prodTime + unprodTime),
+                    normal:  {fill: "#5ecc50"},
+                }, 
+                {
+                    x: "unproductive", 
+                    value: unprodTime/(prodTime + unprodTime),
+                    normal:  {fill: "#f52707"},
+                },
+            ];
 
-        // set the chart title
-        chart.title("Productivity Time Analysis Chart");
+            // create the chart
+            var chart = anychart.pie();
 
-        // add the data
-        chart.data(data);
+            // set the chart title
+            chart.title("Productivity Time Analysis Chart");
 
-        // display the chart in the container
-        chart.container('container');
-        chart.draw();
+            // add the data
+            chart.data(data);
+
+            // display the chart in the container
+            chart.container('container');
+            chart.draw();
         });
     });
 });
